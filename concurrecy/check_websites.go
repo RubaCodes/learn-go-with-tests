8c07d72ebd9f928035dc02de29611c95a9a2c0fa
rubacodes <rubacodes@gmail.com>
2026-01-02 13:01:32 +0100

    CH10-Concurrency

---
+++ concurrecy/check_websites.go
@@ -0,0 +1,27 @@
+package concurrecy
+
+
+type WebsiteChecker func(string) bool
+type result struct{
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func() {
+			//send statement
+			resultChannel <- result{url,wc(url)}
+		}()
+	}
+
+	for range urls {
+		// receive statement
+		r := <- resultChannel
+		results[r.string] = r.bool
+	}
+	return results
+}
