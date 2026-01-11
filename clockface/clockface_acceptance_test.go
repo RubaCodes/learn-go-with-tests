102966dfea426eb55562be59864e827154f494ca
rubacodes <rubacodes@gmail.com>
2026-01-11 09:06:20 +0100

    CH-16: Clockface intermiediate commit

---
+++ clockface/clockface_acceptance_test.go
@@ -0,0 +1,53 @@
+package clockface_test
+
+import (
+	"bytes"
+	"encoding/xml"
+	"slices"
+	"testing"
+	"time"
+
+	"example.com/hello/clockface"
+)
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	clockface.SVGWriter(&b, tm)
+
+	svg := SVG{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	want := Line{150, 150, 150, 60}
+
+	if slices.Contains(svg.Line, want) {
+		return
+	}
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+
+}
+
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
